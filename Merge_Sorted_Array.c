cddb4670e2e7bea030796dd89413c6c76cb69948
Rajasree Laha <laharajasree121@gmail.com>
2024-03-24 19:51:58 +0530

    Create Merge_Sorted_Array.c

---
+++ Merge_Sorted_Array.c
@@ -0,0 +1,24 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i = m - 1; // Pointer for nums1
+    int j = n - 1; // Pointer for nums2
+    int k = m + n - 1; // Pointer for merged array
+    
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+    
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
