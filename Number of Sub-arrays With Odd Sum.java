8d4378c603c29cb3bf95161363f024d42c11fcc8
Rajasree Laha <laharajasree121@gmail.com>
2025-02-25 10:05:41 +0530

    Create Number of Sub-arrays With Odd Sum.java

---
+++ Number of Sub-arrays With Odd Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        int MOD = 1000000007;
+        int odd = 0, even = 1; 
+        int result = 0;
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum % 2 == 1) {
+                result = (result + even) % MOD;
+                odd++;
+            } else {
+                result = (result + odd) % MOD;
+                even++;
+            }
+        }
+
+        return result;
+    }
+}
